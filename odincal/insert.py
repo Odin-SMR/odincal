c830e03dfc48b89d3ab2d832cf6442087d00dd66
Joakim Möller <joakim.moller@molflow.com>
2012-03-29 10:29:58 +0200

    rewriting level0 importer scripts

--- odincal/insert.py
+++ odincal/insert.py
@@ -1,31 +1,24 @@
-from pg import connect
+from pg import DB
 from sys import argv
-from odincal import getAC,getHK
+from odincal.level0 import getAC
+from oops.level0 import ACfile
 from os.path import splitext
+
+class db(DB):
+    def __init__(self):
+        DB.__init__(self,dbname='odin')
+
 
 def files2db():
     if len(argv)>1:
-        con = connect('odin')
+        con = db()
         for datafile in argv[1:]:
             extension = splitext(datafile)[1]
             if extension == '.ac1' or extension == '.ac2':
-                f = open(datafile,'rb')
+                f = ACfile(datafile)
                 while 1:
                     try:
-                        ac = getAC(f)
+                        datadict = getAC(f)
+                        con.insert('ac_level0',datadict)
                     except EOFError:
                         break
-                    q = ac.insert_statement()
-                    con.query(q)
-                f.close()
-            elif extension == '.shk':
-                f = open(datafile,'rb')
-                while 1:
-                    try:
-                        hk = getHK(f)
-                    except EOFError:
-                        break
-                    q = hk.insert_statement()
-                    con.query(q)
-                f.close()
-
