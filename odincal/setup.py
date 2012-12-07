742214c9960fec4b28e2533bf906d4a0e508eca1
Bengt Rydberg <bengt@harrisburg.(none)>
2012-12-07 11:10:46 +0100

    minor change in scripts so they do not crash when we should not process data due to that data is bad

--- odincal/setup.py
+++ odincal/setup.py
@@ -5,7 +5,7 @@ from os.path import join
 
 setup(
     name='odincal',
-    version='0.1.6',
+    version='0.1.9',
     description = 'Odin calibration project',
     long_description=open("README.txt").read() + "\n" +
     open(join("docs", "CHANGELOG.txt")).read(),
