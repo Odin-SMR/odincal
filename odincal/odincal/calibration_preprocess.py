2c4b7c2ec406b0ea072e13c2b0f4f74e580f4eaf
Bengt Rydberg <bengt@harrisburg.(none)>
2013-09-26 09:50:16 +0200

    added new files that are used for performing frequency and intensity calibration of odin data. The files replaces the level1b_window_importer.py that was very large and not easy to understand. The calibration process is controlled by the level1b_window_importer function in level1b_window_importer2.py. frequency_calibration.py contains a class for frequency_calibration. intensity_calibration.py contains a class for intensity_calibration. calibration_preprocess.py contains a class for preprocessing.

---
+++ odincal/odincal/calibration_preprocess.py
@@ -0,0 +1,235 @@
+import copy
+import numpy
+from odincal.ac_level1a_importer import ac_level1a_importer
+from odincal.att_level1_importer import att_level1_importer
+from odincal.shk_level1_importer import shk_level1_importer
+import pg
+from pg import DB
+from sys import stderr,stdout,stdin,argv,exit
+
+class db(DB):
+    def __init__(self):
+        DB.__init__(self,dbname='odin',user='odinop',host='malachite.rss.chalmers.se',passwd='0d!n-cth')
+        #DB.__init__(self,dbname='odin_test')
+
+
+
+class Prepare_data():
+    '''prepare level0 database data for calibration''' 
+    def __init__(self,acfile,backend,version,con): 
+        self.acfile=acfile
+        self.backend=backend
+        self.con=con
+        self.version=version
+    def get_stw_from_acfile(self):
+        temp=[self.acfile]
+        query=self.con.query('''select min(stw),max(stw)
+                       from ac_level0 where file='{0}' '''.format(*temp))
+        result=query.dictresult()
+        if result[0]['max']==None:
+	    #no data from file imported in ac_level0 table
+            return -1,-1
+        else:
+            return result[0]['min'],result[0]['max']
+        
+    def get_soda_version(self,stw1,stw2):
+        temp=[stw1,stw2]
+        query=self.con.query('''select soda 
+                       from attitude_level0 where stw>{0} and stw<{1} 
+                       group by soda'''.format(*temp))
+        result=query.dictresult()
+        if result==[]:
+            return -1
+        else:
+            return result[0]['soda'] 
+    def att_level1_process(self,stw1,stw2,sodaversion):
+        error=att_level1_importer(stw1,stw2,sodaversion,self.backend)
+        return error
+    def shk_level1_process(self,stw1,stw2):
+        error=shk_level1_importer(stw1,stw2,self.backend)
+        return error
+    def ac_level1a_process(self,stw1,stw2):
+        error=ac_level1a_importer(stw1,stw2,self.backend)
+        return error
+    def get_scan_starts(self,stw1,stw2):
+        temp=[stw1,stw2]
+        if self.backend=='AC1':
+            query=self.con.query('''select start,ssb_att from ac_level0 
+                    natural join getscansac1({0},{1}) 
+                    join shk_level1 using(stw,backend)
+                    where start>={0} and start<={1}
+                    and backend='AC1' group by start,ssb_att 
+                    order by start'''.format(*temp))
+            
+        else:
+            query=self.con.query('''select start,ssb_att from ac_level0 
+                    natural join getscansac2({0},{1})
+                    join shk_level1 using(stw,backend)
+                    where start>={0} and start<={1}
+                    and backend='AC2' group by start,ssb_att 
+                    order by start'''.format(*temp))
+        result=query.dictresult()
+        return result
+
+    def get_data_for_calibration(self,stw1,stw2,tdiff,sodaversion):
+        temp=[stw1-tdiff,stw2+tdiff,sodaversion]
+        if self.backend=='AC1':
+
+            query=self.con.query('''(
+                       select ac_level0.stw,start,ssb_att,skybeamhit,cc,
+                       ac_level0.backend,
+                       frontend,sig_type,
+                       spectra,inttime,qerror,qachieved,
+                       latitude,longitude,altitude,lo,ssb,
+                       mixc,imageloadb,imageloada,hotloada,hotloadb,
+                       ssb_fq,mech_type,vgeo,mode,
+                       frontendsplit
+                       from ac_level0
+                       natural join getscansac1({0},{1})
+                       join ac_level1a using (backend,stw)
+                       join shk_level1 using (backend,stw)
+                       join attitude_level1 using (backend,stw)
+                       where ac_level0.stw>={0} and ac_level0.stw<={1}
+                       and ac_level0.backend='AC1' and soda={2} and lo>1 
+                       order by stw)'''.format(*temp))
+      
+
+        if self.backend=='AC2':
+        
+            query=self.con.query('''(
+                       select ac_level0.stw,start,ssb_att,skybeamhit,cc,
+                       ac_level0.backend,
+                       frontend,sig_type,
+                       spectra,inttime,qerror,qachieved,
+                       latitude,longitude,altitude,lo,ssb,
+                       mixc,imageloadb,imageloada,hotloada,hotloadb,
+                       ssb_fq,mech_type,vgeo,mode,
+                       frontendsplit
+                       from ac_level0
+                        natural join getscansac2({0},{1})
+                       join ac_level1a using(stw,backend)
+                       join shk_level1 using(stw,backend)
+                       join attitude_level1 using(stw,backend)
+                       where ac_level0.stw>={0} and ac_level0.stw<={1}
+                       and ac_level0.backend='AC2' and soda={2}
+                       and lo>1 
+                       order by stw)'''.format(*temp))
+            
+        result=query.dictresult()
+        print len(result)
+        return result
+
+    def filter_data(self,result,start,ssb_att,scanfrontend,tdiff):
+        spectra=[]
+        remove_next_ref=0
+        calspec=0
+        start0=0
+        for row in result:
+
+            if row['frontendsplit']!=scanfrontend:
+                continue
+            if row['start']>=start-tdiff and row['start']<=start+tdiff:
+                pass
+            else:
+                continue
+            if row['ssb_att']!=ssb_att:
+                continue
+            
+            if row['start']>start0:
+                start0=row['start']
+                calspec=0
+
+            #remove sky beam references
+            if row['sig_type']=='REF' and row['mech_type']=='SK2':
+                remove_next_ref=1
+                continue
+            if row['sig_type']=='REF' and row['mech_type']=='REF':
+                remove_next_ref=1
+                continue
+            #remove data that where skybeam1 interfere with objects
+            EARTH1=0x0001
+            MOON1=0x0002
+            GALAX1=0x0004
+            SUN1=0x0008
+            if  ( (row['sig_type']=='REF' and row['mech_type']=='SK1') 
+                and
+                (  ( row['skybeamhit'] & EARTH1==EARTH1) or 
+                   ( row['skybeamhit'] & MOON1==MOON1) or 
+                   ( row['skybeamhit'] & SUN1==SUN1)
+                   )):
+                continue
+
+            #remove first refrence data after a calibration
+            if row['sig_type']=='REF' and row['mech_type']=='CAL':
+                remove_next_ref=1
+            #do not use SK1 signals when the earlier
+            #reference signal was from another source
+            if (row['sig_type']=='REF' and row['mech_type']=='SK1'
+                and remove_next_ref==1):
+                remove_next_ref=0
+                continue
+            
+            #remove unwished calibration spectrum
+            #keep only the second 
+            if (row['sig_type']=='REF' and row['mech_type']=='CAL'):
+                calspec=calspec+1
+                if calspec==2:
+                    row['sig_type']='CAL'
+                else:
+                    continue
+            spectra.append(row)
+        return spectra
+
+
+def main():
+    '''prepare data for calibration'''
+     
+    #acfile='100b8721.ac1'
+    #backend='AC1'
+    acfile=argv[1]
+    backend=argv[2]
+
+    con=db()
+
+    p=Prepare_data(acfile,backend,version,con)
+    
+    #find out max and min stw from acfile to calibrate
+    stw1,stw2=p.get_stw_from_acfile()
+    if stw1==-1:
+        info={'info': 'no ac data',
+              'total_scans': 0,
+              'success_scans': 0,
+              'version': version}
+        print info
+        return   
+
+    #find out which sodaversion we have for this data
+    sodaversion=p.get_soda_version(stw1,stw2) 
+    if sodaversion==-1:
+        info={'info': 'no attitude data',
+              'total_scans': 0,
+              'success_scans': 0,
+              'version': version}
+        print info
+        return
+
+    #perform level0 data processing
+    if level0_process==1:
+       error=p.att_level1_process(stw1,stw2,sodaversion)
+       if error==1:
+           print {'info': 'pg problem'}
+           return
+       error=p.shk_level1_process(stw1,stw2)
+       if error==1:
+           print {'info': 'pg problem'}
+           return
+       error=p.ac_level1a_process(stw1,stw2)
+       if error==1:
+           print {'info': 'pg problem'}
+           return
+    
+       con.close()
+   
+
+if __name__=='__main__':
+    main()
---
+++ odincal/odincal/frequency_calibration.py
@@ -0,0 +1,188 @@
+import numpy
+
+
+class Spectra: 
+    """A class derived to perform frequency calibration of odin spectra"""
+    def __init__(self,con,data,ref):
+        self.ref=ref
+        self.start=data['start']
+        self.data=numpy.ndarray(shape=(112*8,),
+                     dtype='float64',buffer=con.unescape_bytea(
+                     data['spectra']))
+        self.stw=data['stw']
+        self.LO=eval(data['ssb_fq'].replace('{','(').replace('}',')'))
+        self.backend=data['backend']
+        self.frontend=data['frontend']
+        self.vgeo=data['vgeo']
+        self.mode=data['mode']
+        self.tcal=data['hotloada']
+        if self.tcal==0:
+            self.tcal=data['hotloadb']
+        self.freqres=1e6
+        if data['sig_type']=='SIG':
+            self.qerror=eval(data['qerror'].replace('{','(').replace('}',')'))
+            self.qachieved=eval(data['qachieved'].replace('{','(').replace('}',')'))
+        self.inttime=data['inttime']
+        self.intmode=511
+        self.skyfreq=0
+        self.lofreq=data['lo']
+        self.ssb=data['ssb']
+        self.Tpll=data['imageloadb']
+        if self.Tpll==0:
+            self.Tpll=data['imageloada'] 
+        self.current=data['mixc']
+        self.type=data['sig_type']
+        self.source=[]
+        self.topic=[]
+        self.restfreq=[]
+        self.latitude=data['latitude']
+        self.longitude=data['longitude']
+        self.altitude=data['altitude']
+        self.tsys=0
+        self.efftime=0
+        self.sbpath=0
+        self.cc=numpy.ndarray(shape=(8,96),dtype='float64',
+                              buffer=con.unescape_bytea(data['cc']))
+        self.zerolag=numpy.array(self.cc[0:8,0])
+        self.skybeamhit=data['skybeamhit']
+        self.ssb_att=eval(data['ssb_att'].replace('{','(').replace('}',')'))
+    def tuning(self):
+
+        if self.frontend == '119':
+            IFfreq = 3900.0e6
+            self.lofreq = 114.8498600000000e+9
+            self.fcalibrate(self.lofreq,IFfreq)
+            return
+        
+        rxs = { '495': 1, '549': 2, '555': 3, '572': 4 }
+        if not self.frontend in rxs.keys():
+            return 
+        (IFfreq,sbpath) = self.getSideBand(self.frontend, self.lofreq, self.ssb)
+        self.sbpath = sbpath/1.0e6
+        (ADC_SPLIT, ADC_UPPER) = (0x0200, 0x0400)
+        if self.intmode & ADC_SPLIT:
+            if self.backend == 'AC1':
+                if self.intmode & ADC_UPPER: 
+                    IFfreq = IFfreq*3.6/3.9
+                else:                     
+                    IFfreq = IFfreq*4.2/3.9
+            elif self.backend == 'AC2':
+                if self.intmode & ADC_UPPER: 
+                    IFfreq = IFfreq*4.2/3.9
+                else:                     
+                    IFfreq = IFfreq*3.6/3.9
+                
+        if self.current < 0.25:
+            #odin.Warn("low mixer current %5.2f" % (current))
+            if self.frontend <> '572':
+                self.lofreq = 0.0
+        #else:
+        #    IFfreq = 0.0
+           #odin.Warn("LO frequency lookup failed")
+        self.fcalibrate(self.lofreq, IFfreq)
+
+         
+    def fcalibrate(self,LOfreq, IFfreq):
+        """Perform frequency calibration."""
+        if LOfreq == 0.0: 
+            return
+        if self.frontend == '495' or self.frontend == '549':
+            drift = 1.0+(29.23-0.138*self.Tpll)*1.0e-6
+        else:
+            drift = 1.0+(24.69-0.109*self.Tpll)*1.0e-6
+        LOfreq = LOfreq*drift
+        self.lofreq  = LOfreq
+        self.skyfreq = LOfreq+IFfreq
+        self.maxsup  = LOfreq-IFfreq
+        # apply Doppler correction
+        self.restfreq = self.skyfreq/(1.0 - self.vgeo/2.99792456e8);
+       
+ 
+    def getSideBand(self,rx, LO, ssb):
+        SSBparams = {
+            '495': ( 61600.36, 104188.89, 0.0002977862, 313.0 ),
+            '549': ( 57901.86, 109682.58, 0.0003117128, 313.0 ),
+            '555': ( 60475.43, 116543.50, 0.0003021341, 308.0 ),
+            '572': ( 58120.92, 115256.73, 0.0003128605, 314.0 )}
+        d = 0.0
+        C1 = SSBparams[rx][0]
+        C2 = SSBparams[rx][1]
+        sf = SSBparams[rx][2]
+        sbpath = (-SSBparams[rx][3]+2.0*SSBparams[rx][3]*ssb/4095.0)*2.0
+        for i in range(-2,3):
+            s3900 = 299.79/(ssb + C1)*(C2 + i/sf)-LO/1.0e9;
+            if abs(abs(s3900)-3.9) < abs(abs(d)-3.9):
+                d = s3900;
+        if d < 0.0: 
+            IFfreq = -3900.0e6
+        else:       
+            IFfreq =  3900.0e6
+        return (IFfreq,sbpath)
+
+    def freqMode(self):
+        df = 30.0e6
+        self.freqmode = 0
+        self.split = 0
+        LO = self.lofreq*(1.0 + self.vgeo/2.99792456e8)
+        modes = {
+            'STRAT':  "Stratospheric",
+            'ODD_H':  "Odd hydrogen",
+            'ODD_N':  "Odd nitrogen",
+            'WATER':  "Water isotope",
+            'SUMMER': "Summer mesosphere",
+            'DYNAM':  "Transport"
+            }
+
+        config = None
+        if self.backend == 'AC1':
+            if self.frontend == '495':
+                config = [[492.750e9,23, "DYNAM", 0],
+                          [495.402e9,29, "DYNAM", 0],
+                          [499.698e9,25, "DYNAM", 0]]
+            elif self.frontend == '549':
+                config = [[548.502e9, 2, "STRAT", 0],
+                          [553.050e9,19, "WATER", 0],
+                          [547.752e9,21, "WATER", 1],
+                          [553.302e9,23, "DYNAM", 0],
+                          [553.302e9,29, "DYNAM", 0]]
+            elif self.frontend == '555':
+                config = [[553.298e9,13, "SUMMER", 0]]
+            elif self.frontend == '572':
+                config = [[572.762e9,24, "DYNAM", 0]]
+        elif self.backend == 'AC2':
+            if self.frontend == '495':
+                config = [[497.880e9, 1, "STRAT", 1],
+                          [492.750e9, 8, "WATER", 1],
+                          [494.250e9,17, "WATER", 0],
+                          [499.698e9,25, "DYNAM", 0]]
+            elif self.frontend == '572':
+                config = [[572.964e9,22, "DYNAM", 1],
+                          [572.762e9,14,"SUMMER", 0]]
+        if config:
+            df = [0.0]*len(config)
+            for i in range(len(df)):
+                df[i] = abs(LO-config[i][0])
+            i = df.index(min(df))
+            # print "configuration", i, config[i]
+            self.freqmode = config[i][1]
+            self.topic    = config[i][2]
+            self.split    = config[i][3]
+            # print "configuration %s:%s:%10.1f" % \
+            #       (s.backend, s.frontend, LO/1.0e6),
+            # print " %d, %s" % (self.freqmode, self.topic)
+        else:
+            odin.Warn("unknown configuration %s:%s:%10.1f" % \
+                      (s.backend, s.frontend, LO/1.0e6))
+
+        if self.freqmode:
+            self.source = "%s FM=%d" % (modes[self.topic], self.freqmode)
+        else:
+            self.source ="unknown"
+            self.topic = "N/A"
+        
+       
+
+
+ 
+if __name__ == "__main__":
+    pass
