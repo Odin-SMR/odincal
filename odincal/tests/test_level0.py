98afae069881982a5e4d107bb4e421627b2dcb83
bengt.rydberg <bengt.rydberg@molflow.com>
2021-04-27 16:18:22 +0200

    adding test case

--- odincal/tests/test_level0.py
+++ odincal/tests/test_level0.py
@@ -5,5 +5,7 @@ def test_correction():
     for file_set in [
             ('017f6e6c.ac2', 0 * 2**32),
             ('11f1eafe.fba', 1 * 2**32),
-            ('20683ad2.ac2', 2 * 2**32)]:
+            ('20683ad2.ac2', 2 * 2**32),
+            ('30683ad2.ac2', 3 * 2**32),
+        ]:
         assert stw_correction(file_set[0]) == file_set[1]
